b97fc40803a56396876e5ddf2c3cc9cf63715639
Quentin Mathis <quentin.mathis@gmail.com>
2015-03-10 17:13:59 +0100

    ss

--- src/Composer/Installers/ZeusInstaller.php
+++ src/Composer/Installers/ZeusInstaller.php
@@ -6,4 +6,16 @@ class ZeusInstaller extends BaseInstaller
     protected $locations = array(
         'module'    => 'app/modules/{$name}/',
     );
+
+    public function getInstallPath(PackageInterface $package, $frameworkType = '')
+    {
+        if( preg_match('#(.*)/zeus-module-(.*)#',$package->getName(),$aMatches) )
+        {
+            return $this->templatePath($this->locations['module'], array('name' => $aMatches[2]) );
+        }
+        else
+        {
+            return parent::getInstallPath($package, $frameworkType);
+        }
+    }
 }
