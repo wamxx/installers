1de37fb9ea23f331d244f8a2b5c19d0177edafb7
Quentin Mathis <quentin.mathis@gmail.com>
2015-03-10 20:38:18 +0100

    ef

--- src/Composer/Installers/ZeusInstaller.php
+++ src/Composer/Installers/ZeusInstaller.php
@@ -9,10 +9,26 @@ class ZeusInstaller extends BaseInstaller
 
     public function getInstallPath(\Composer\Package\PackageInterface $package, $frameworkType = '')
     {
-        var_dump($package->getExtra());
-        exit;
         if( preg_match('#(.*)/zeus-module-(.*)#',$package->getName(),$aMatches) )
         {
+            $aExtra = $package->getExtra();
+
+            $sModuleFile = '';
+            if( file_exists($sModuleFile) )
+            {
+                $aModules = include $sModuleFile;
+                $aModules[$package->getName()] = [
+                    'className' => $aExtra['className'],
+                    'path' => '../app/modules/'.$aMatches[2],
+                ];
+                file_put_contents($sModuleFile,'return '.var_export($aModules,true));
+            }
+            else
+            {
+
+            }
+
+
             return $this->templatePath($this->locations['module'], array('name' => $aMatches[2]) );
         }
         else
