075f7605343b1d52a7989537b08946d67e995358
Quentin Mathis <quentin.mathis@gmail.com>
2015-03-11 17:38:38 +0100

    ee

--- src/Composer/Installers/ZeusPackage.php
+++ src/Composer/Installers/ZeusPackage.php
@@ -24,6 +24,11 @@ class ZeusPackage
   public static function postPackageInstall(\Composer\Script\PackageEvent $event) {
     $installedPackage = $event->getComposer()->getPackage();
     // any tasks to run after the package is installed?
+    $io = $event->getIO();
+    if ($io->askConfirmation("Are you sure you want to proceed? ", false)) {
+      // ok, continue on to composer install
+      return true;
+    }
   }
 
   public static function postPackageUninstall(\Composer\Script\PackageEvent $event) {
